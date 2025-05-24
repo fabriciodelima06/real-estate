0c2da534c6d6036e9a37e8c045691b6a8508fe5a
Fabrício Aniceto de Lima <fabricio.lima@emap.ma.gov.br>
2025-05-24 11:15:50 -0300

    Correção de bugs

--- src/app/(Mainbody)/demo/pages/other-pages/about-us-2/page.js
+++ src/app/(Mainbody)/demo/pages/other-pages/about-us-2/page.js
@@ -2,14 +2,14 @@
 import React, { Fragment } from "react";
 import NavbarThree from "@/layout/headers/NavbarThree";
 import FooterThree from "@/layout/footers/FooterThree";
-import BreadCrumb2 from "@/layout/Breadcrumb/Breadcrumb2";
+import Breadcrumb2 from "@/layout/Breadcrumb/Breadcrumb2";
 import BodyContent from "@/components/pages/other-pages/aboutUs2";
 
 const AboutUs2 = () => {
   return (
     <Fragment>
       <NavbarThree />
-      <BreadCrumb2 />
+      <Breadcrumb2 />
       <BodyContent />
       <FooterThree />
     </Fragment>
